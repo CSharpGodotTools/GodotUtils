41557debefc2fb94eafb51ee62a19ad07490edfc
Valk <sebastianbelle074@protonmail.com>
2025-08-27 21:09:56 -0500

    Set game scene to res://Level.tscn

--- Scenes/Options/Scripts/OptionsGraphics.cs
+++ Scenes/Options/Scripts/OptionsGraphics.cs
@@ -8,7 +8,6 @@ public partial class OptionsGraphics : Control
 {
     public event Action<int> AntialiasingChanged;
 
-    private GraphicsSetting[] _graphicsSettings = [];
     private ResourceOptions _options;
     private OptionButton _antialiasing;
 
@@ -16,10 +15,8 @@ public partial class OptionsGraphics : Control
     {
         _options = OptionsManager.GetOptions();
 
-        InitializeSettingsMetadata();
         SetupQualityPreset();
         SetupAntialiasing();
-        SetupWorldEnvironmentSettings();
     }
 
     private void _OnQualityModeItemSelected(int index)
@@ -31,61 +28,6 @@ public partial class OptionsGraphics : Control
     {
         _options.Antialiasing = index;
         AntialiasingChanged?.Invoke(index);
-    }
-
-    private void InitializeSettingsMetadata()
-    {
-        _graphicsSettings =
-        [
-            CreateGlowSetting(),
-            CreateAmbientOcclusionSetting(),
-            CreateIndirectLightingSetting(),
-            CreateReflectionsSetting()
-        ];
-    }
-
-    private GraphicsSetting CreateGlowSetting()
-    {
-        return new()
-        {
-            Name = "GLOW",
-            GetOption = () => _options.Glow,
-            SetOption = val => _options.Glow = val,
-            ApplyInGame = (env, val) => env.GlowEnabled = val
-        };
-    }
-
-    private GraphicsSetting CreateAmbientOcclusionSetting()
-    {
-        return new()
-        {
-            Name = "AMBIENT_OCCLUSION",
-            GetOption = () => _options.AmbientOcclusion,
-            SetOption = val => _options.AmbientOcclusion = val,
-            ApplyInGame = (env, val) => env.SsaoEnabled = val
-        };
-    }
-
-    private GraphicsSetting CreateIndirectLightingSetting()
-    {
-        return new()
-        {
-            Name = "INDIRECT_LIGHTING",
-            GetOption = () => _options.IndirectLighting,
-            SetOption = val => _options.IndirectLighting = val,
-            ApplyInGame = (env, val) => env.SsilEnabled = val
-        };
-    }
-
-    private GraphicsSetting CreateReflectionsSetting()
-    {
-        return new()
-        {
-            Name = "REFLECTIONS",
-            GetOption = () => _options.Reflections,
-            SetOption = val => _options.Reflections = val,
-            ApplyInGame = (env, val) => env.SsrEnabled = val
-        };
     }
 
     private void SetupQualityPreset()
@@ -100,43 +42,6 @@ public partial class OptionsGraphics : Control
         _antialiasing.Select(_options.Antialiasing);
     }
 
-    private void SetupWorldEnvironmentSettings()
-    {
-        foreach (GraphicsSetting setting in _graphicsSettings)
-        {
-            AddSettingControl(setting);
-        }
-    }
-
-    private void AddSettingControl(GraphicsSetting setting)
-    {
-        HBoxContainer hbox = new();
-
-        Label label = CreateLabel(setting.Name);
-        hbox.AddChild(label);
-
-        CheckBox checkBox = CreateCheckBox(setting);
-        setting.CheckBoxControl = checkBox;
-        checkBox.Pressed += () => OnSettingPressed(setting);
-        hbox.AddChild(checkBox);
-
-        AddChild(hbox);
-    }
-
-    private void OnSettingPressed(GraphicsSetting setting)
-    {
-        bool pressed = setting.CheckBoxControl!.ButtonPressed;
-
-        setting.SetOption(pressed);
-
-        GodotUtils.UI.PopupMenu popupMenu = Services.Get<GodotUtils.UI.PopupMenu>();
-
-        if (popupMenu?.WorldEnvironment == null)
-            return;
-
-        setting.ApplyInGame(popupMenu.WorldEnvironment.Environment, pressed);
-    }
-
     private static Label CreateLabel(string text)
     {
         return new Label
@@ -144,24 +49,6 @@ public partial class OptionsGraphics : Control
             Text = text,
             CustomMinimumSize = new Vector2(200, 0)
         };
-    }
-
-    private static CheckBox CreateCheckBox(GraphicsSetting setting)
-    {
-        return new CheckBox()
-        {
-            ButtonPressed = setting.GetOption()
-        };
-    }
-
-    private class GraphicsSetting
-    {
-        public required string Name { get; init; }
-        public required Func<bool> GetOption { get; init; }
-        public required Action<bool> SetOption { get; init; }
-        public required Action<Environment, bool> ApplyInGame { get; init; }
-
-        public CheckBox CheckBoxControl;
     }
 }
 
