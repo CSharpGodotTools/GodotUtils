efb8f52b1609adb9111b43fb5125a1ac6ae5e8f9
Valk <sebastianbelle074@protonmail.com>
2025-08-31 12:34:16 -0500

    Split SetCollisionLayerAndMask into 2 functions

--- Extensions/ExtensionsCollisionObject2D.cs
+++ Extensions/ExtensionsCollisionObject2D.cs
@@ -7,9 +7,13 @@ public static class ExtensionsCollisionObject2D
     /// <summary>
     /// Set the <paramref name="layers"/> for CollisionLayer and CollisionMask
     /// </summary>
-    public static void SetCollisionLayerAndMask(this CollisionObject2D collisionObject, params int[] layers)
+    public static void SetCollisionLayer(this CollisionObject2D collisionObject, params int[] layers)
     {
         collisionObject.CollisionLayer = (uint)MathUtils.GetLayerValues(layers);
+    }
+
+    public static void SetCollisionMask(this CollisionObject2D collisionObject, params int[] layers)
+    {
         collisionObject.CollisionMask = (uint)MathUtils.GetLayerValues(layers);
     }
 }
