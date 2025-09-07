571a3af80a2d5525ca831e4b6a353d514a72f088
Valk <sebastianbelle074@protonmail.com>
2025-09-07 17:12:39 -0500

    tech: Rename SetCollisionLayer SetCollisionMask

--- Extensions/ExtensionsCollisionObject2D.cs
+++ Extensions/ExtensionsCollisionObject2D.cs
@@ -7,12 +7,12 @@ public static class ExtensionsCollisionObject2D
     /// <summary>
     /// Set the <paramref name="layers"/> for CollisionLayer and CollisionMask
     /// </summary>
-    public static void SetCollisionLayer(this CollisionObject2D collisionObject, params int[] layers)
+    public static void SetCollisionLayers(this CollisionObject2D collisionObject, params int[] layers)
     {
         collisionObject.CollisionLayer = (uint)MathUtils.GetLayerValues(layers);
     }
 
-    public static void SetCollisionMask(this CollisionObject2D collisionObject, params int[] layers)
+    public static void SetCollisionMasks(this CollisionObject2D collisionObject, params int[] layers)
     {
         collisionObject.CollisionMask = (uint)MathUtils.GetLayerValues(layers);
     }
