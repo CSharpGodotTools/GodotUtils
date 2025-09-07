0f83ac65159ed3643c9fe55563763e06e0bcb8d2
Valk <sebastianbelle074@protonmail.com>
2025-09-07 17:33:53 -0500

    tech: Add more extension methods

--- Extensions/ExtensionsCollisionObject2D.cs
+++ Extensions/ExtensionsCollisionObject2D.cs
@@ -4,9 +4,6 @@ namespace GodotUtils;
 
 public static class ExtensionsCollisionObject2D
 {
-    /// <summary>
-    /// Set the <paramref name="layers"/> for CollisionLayer and CollisionMask
-    /// </summary>
     public static void SetCollisionLayers(this CollisionObject2D collisionObject, params int[] layers)
     {
         collisionObject.CollisionLayer = (uint)MathUtils.GetLayerValues(layers);
@@ -15,5 +12,15 @@ public static class ExtensionsCollisionObject2D
     public static void SetCollisionMasks(this CollisionObject2D collisionObject, params int[] layers)
     {
         collisionObject.CollisionMask = (uint)MathUtils.GetLayerValues(layers);
+    }
+
+    public static void ClearCollisionLayers(this CollisionObject2D collisionObject2D)
+    {
+        collisionObject2D.CollisionLayer = 0;
+    }
+
+    public static void ClearCollisionMasks(this CollisionObject2D collisionObject2D)
+    {
+        collisionObject2D.CollisionMask = 0;
     }
 }
