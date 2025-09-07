fe8bd8ccacfeceea3dbb7b71d556d85b85d0ec8a
Valk <sebastianbelle074@protonmail.com>
2025-09-07 17:42:53 -0500

    tech: Add Area3D Extensions

---
+++ Extensions/Area3DExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Godot;
+
+namespace GodotUtils;
+
+public static class Area3DExtensions
+{
+    public static void SetMonitoringDeferred(this Area3D area, bool enabled)
+    {
+        area.SetDeferred(Area3D.PropertyName.Monitoring, enabled);
+    }
+
+    public static void SetMonitorableDeferred(this Area3D area, bool enabled)
+    {
+        area.SetDeferred(Area3D.PropertyName.Monitorable, enabled);
+    }
+}
