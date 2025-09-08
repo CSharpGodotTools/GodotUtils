7917d3ca6e9c10641139f900c1f463eb9a58ad83
Valk <sebastianbelle074@protonmail.com>
2025-09-08 01:29:04 -0500

    tech: Add gpu particle factory classes

---
+++ Factories/GpuParticle2DFactory.cs
@@ -0,0 +1,15 @@
+﻿using Godot;
+
+namespace GodotUtils;
+
+public static class GpuParticle2DFactory
+{
+    public static GpuParticles2D OneShot(Node parent, PackedScene particleScene)
+    {
+        GpuParticles2D particles = particleScene.Instantiate<GpuParticles2D>();
+        particles.OneShot = true;
+        particles.Finished += particles.QueueFree;
+        parent.AddChild(particles);
+        return particles;
+    }
+}
---
+++ Factories/GpuParticle3DFactory.cs
@@ -0,0 +1,15 @@
+﻿using Godot;
+
+namespace GodotUtils;
+
+public static class GpuParticle3DFactory
+{
+    public static GpuParticles3D OneShot(Node parent, PackedScene particleScene)
+    {
+        GpuParticles3D particles = particleScene.Instantiate<GpuParticles3D>();
+        particles.OneShot = true;
+        particles.Finished += particles.QueueFree;
+        parent.AddChild(particles);
+        return particles;
+    }
+}
