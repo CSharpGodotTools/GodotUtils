e7776bc39431f1b88b18a222a67c624f98607a42
Valk <sebastianbelle074@protonmail.com>
2025-11-29 14:03:28 -0600

    Add script to remove orphaned cs uid files

--- Utils/ToolScriptHelpers.cs
+++ Utils/ToolScriptHelpers.cs
@@ -1,4 +1,5 @@
 using Godot;
+using System.IO;
 
 namespace GodotUtils.UI;
 
@@ -12,6 +13,13 @@ public partial class ToolScriptHelpers : Node
         set => DeleteEmptyFolders();
     }
 
+    [Export]
+    public bool RemoveOrphanedCSUIDFiles
+    {
+        get => false;
+        set => DeleteOrphanedCSUIDFiles();
+    }
+
     private static void DeleteEmptyFolders()
     {
         if (!Engine.IsEditorHint()) // Do not trigger on game build
@@ -20,5 +28,30 @@ public partial class ToolScriptHelpers : Node
         DirectoryUtils.DeleteEmptyDirectories("res://");
 
         GD.Print("Removed all empty folders from the project. Restart the game engine or wait some time to see the effect.");
+    }
+
+    private static void DeleteOrphanedCSUIDFiles()
+    {
+        if (!Engine.IsEditorHint())
+            return;
+
+        string projectPath = ProjectSettings.GlobalizePath("res://");
+
+        int deletedCount = 0;
+
+        foreach (string file in Directory.GetFiles(projectPath, "*.cs.uid", SearchOption.AllDirectories))
+        {
+            string directory = Path.GetDirectoryName(file);
+            string baseName = Path.GetFileNameWithoutExtension(file).Replace(".cs", ""); // Remove ".cs" from ".cs.uid"
+            string csFile = Path.Combine(directory, baseName + ".cs");
+
+            if (!File.Exists(csFile))
+            {
+                File.Delete(file);
+                deletedCount++;
+            }
+        }
+
+        GD.Print($"Deleted {deletedCount} orphaned .cs.uid files.");
     }
 }
