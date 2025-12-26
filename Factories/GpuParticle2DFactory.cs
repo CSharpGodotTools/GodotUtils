4a8237df0019c784f273faf8fa68f74f4e2ccf46
Valk <sebastianbelle074@protonmail.com>
2025-12-25 18:08:19 -0600

    Add documentation to GpuParticle2DFactory.cs

--- Factories/GpuParticle2DFactory.cs
+++ Factories/GpuParticle2DFactory.cs
@@ -1,9 +1,16 @@
-﻿using Godot;
+using Godot;
 
 namespace GodotUtils;
 
 public static class GpuParticle2DFactory
 {
+    /// <summary>
+    /// Instantiates a GPUParticles2D scene as a one-shot effect, frees it automatically when
+    /// emission finishes, and adds it as a child of the given parent node.
+    /// </summary>
+    /// <param name="parent">The node that will own the instantiated particle node.</param>
+    /// <param name="particleScene">A PackedScene that instantiates to a GPUParticles2D.</param>
+    /// <returns>The instantiated and configured GPUParticles2D.</returns>
     public static GpuParticles2D OneShot(Node parent, PackedScene particleScene)
     {
         GpuParticles2D particles = particleScene.Instantiate<GpuParticles2D>();
@@ -13,6 +20,13 @@ public static class GpuParticle2DFactory
         return particles;
     }
 
+    /// <summary>
+    /// Instantiates a GPUParticles2D scene as a looping effect, does not automatically free it,
+    /// and adds it as a child of the given parent node.
+    /// </summary>
+    /// <param name="parent">The node that will own the instantiated particle node.</param>
+    /// <param name="particleScene">A PackedScene that instantiates to a GPUParticles2D.</param>
+    /// <returns>The instantiated and configured GPUParticles2D.</returns>
     public static GpuParticles2D Looping(Node parent, PackedScene particleScene)
     {
         GpuParticles2D particles = particleScene.Instantiate<GpuParticles2D>();
