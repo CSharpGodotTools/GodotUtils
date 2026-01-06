773eab5dc445705069091ee8c8cd6fcf9065b9c5
Valk <sebastianbelle074@protonmail.com>
2026-01-05 19:41:06 -0600

    Update IPoolable.cs

--- Interfaces/IPoolable.cs
+++ Interfaces/IPoolable.cs
@@ -4,7 +4,18 @@ namespace GodotUtils;
 
 public interface IPoolable<TNode> where TNode : CanvasItem, IPoolable<TNode>
 {
+    /// <summary>
+    /// Invoked when a new <typeparamref name="TNode"/> is created.
+    /// </summary>
     void OnCreate(GodotPool<TNode> pool);
+
+    /// <summary>
+    /// Invoked when a <typeparamref name="TNode"/> is aquired with <c><see cref="GodotPool{TNode}"/>.Get()</c>
+    /// </summary>
     void OnAquire();
+
+    /// <summary>
+    /// Invoked when a <typeparamref name="TNode"/> is released from the pool.
+    /// </summary>
     void OnRelease();
 }
