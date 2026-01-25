a3e0bc33e1a40047af4058893e365ab606f96d73
Valk <sebastianbelle074@protonmail.com>
2026-01-25 17:18:25 -0600

    Remove "Create" prefix from all area factory methods

--- Factories/Areas/Core/AreaFactory2D.cs
+++ Factories/Areas/Core/AreaFactory2D.cs
@@ -4,8 +4,8 @@ namespace GodotUtils;
 
 public static class AreaFactory2D
 {
-    public static WorldBoundaryArea2D CreateWorldBoundary(Vector2 normal) => new(normal);
-    public static CapsuleArea2D CreateCapsule(float radius, float height) => new(radius, height);
-    public static CircleArea2D CreateCircle(float radius) => new(radius);
-    public static RectArea2D CreateRect(Vector2 size) => new(size);
+    public static WorldBoundaryArea2D WorldBoundary(Vector2 normal) => new(normal);
+    public static CapsuleArea2D Capsule(float radius, float height) => new(radius, height);
+    public static CircleArea2D Circle(float radius) => new(radius);
+    public static RectArea2D Rect(Vector2 size) => new(size);
 }
--- Factories/Areas/Core/AreaFactory3D.cs
+++ Factories/Areas/Core/AreaFactory3D.cs
@@ -4,9 +4,9 @@ namespace GodotUtils;
 
 public static class AreaFactory3D
 {
-    public static WorldBoundaryArea3D CreateWorldBoundary(Plane plane) => new(plane);
-    public static CylinderArea3D CreateCylinder(float radius, float height) => new(radius, height);
-    public static CapsuleArea3D CreateCapsule(float radius, float height) => new(radius, height);
-    public static SphereArea3D CreateSphere(float radius) => new(radius);
-    public static BoxArea3D CreateBox(Vector3 size) => new(size);
+    public static WorldBoundaryArea3D WorldBoundary(Plane plane) => new(plane);
+    public static CylinderArea3D Cylinder(float radius, float height) => new(radius, height);
+    public static CapsuleArea3D Capsule(float radius, float height) => new(radius, height);
+    public static SphereArea3D Sphere(float radius) => new(radius);
+    public static BoxArea3D Box(Vector3 size) => new(size);
 }
