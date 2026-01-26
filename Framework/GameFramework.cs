4a7351b07215e10ee17e9a31fc5702cbc9934d54
Valk <sebastianbelle074@protonmail.com>
2026-01-26 01:32:28 -0600

    Make GameFramework class static

--- Framework/GameFramework.cs
+++ Framework/GameFramework.cs
@@ -1,14 +1,10 @@
 using GodotUtils.Debugging;
+using GodotUtils.Framework;
 using GodotUtils.Framework.UI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace GodotUtils.Framework;
+namespace GodotUtils;
 
-public class GameFramework
+public static class GameFramework
 {
     public static MetricsOverlay Metrics => AutoloadsFramework.Instance.MetricsOverlay;
     public static OptionsManager Options => AutoloadsFramework.Instance.OptionsManager;
